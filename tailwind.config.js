fbded2ff333a46ef102f60e2b1fda4873a4c16c1
Hồ Hoài Phong <longtam20015@gmail.com>
2023-07-09 20:08:13 +0700

    design login and register

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      color: {
+        orange: '#ee4d2d'
+      }
+    }
   },
   plugins: []
 }
