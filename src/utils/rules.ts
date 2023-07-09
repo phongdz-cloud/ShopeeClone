db4c8640a4fae10536003bfaa2e8961013a37b95
Hồ Hoài Phong <longtam20015@gmail.com>
2023-07-09 20:55:52 +0700

    validate field basic register

---
+++ src/utils/rules.ts
@@ -0,0 +1,52 @@
+import type { RegisterOptions } from 'react-hook-form'
+
+type Rules = { [key in 'email' | 'password' | 'confirm_passowrd']?: RegisterOptions }
+export const rules: Rules = {
+  email: {
+    required: {
+      value: true,
+      message: 'Email là bắt buộc'
+    },
+    pattern: {
+      value:
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      message: 'Email không đúng định dạng'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    }
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'Password là bắt buộc'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    }
+  },
+  confirm_passowrd: {
+    required: {
+      value: true,
+      message: 'Nhập lại password là bắt buộc'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    }
+  }
+}
